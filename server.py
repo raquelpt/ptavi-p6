13cecd923ba52875d308396711fcebd0085c43dd
raquelpt <raquelita_25_4@hotmail.com>
2014-11-24 14:49:51 +0100

    pep8 pasado

--- server.py
+++ server.py
@@ -9,77 +9,78 @@ import sys
 import os
 
 if len(sys.argv) != 4:
-	print "Usage: python server.py IP port audio_file"
-	raise SystemExit
-	
+    print "Usage: python server.py IP port audio_file"
+    raise SystemExit
+
 
 IP = sys.argv[1]
 
-FICHERO_AUDIO = sys.argv[3]	
+FICHERO_AUDIO = sys.argv[3]
 
 # Puerto en el que escuchamos
 
 PORT = int(sys.argv[2])
 
-try: 
-	audio = open (FICHERO_AUDIO, 'r')
+try:
+    audio = open(FICHERO_AUDIO, 'r')
 except IOError:
-	print "Audio file doesn't exist"
-	raise SystemExit
-
-
+    print "Audio file doesn't exist"
+    raise SystemExit
 
 # Damos permiso de ejecución a RTP
+
 os.system("chmod +x mp32rtp")
 
+
 class EchoHandler(SocketServer.DatagramRequestHandler):
-	"""
-   	Echo server class
-  	"""
+    """
+      Echo server class
+    """
 
-	def handle(self):
-		
-		self.wfile.write("Hemos recibido tu peticion")
-		while 1:
-		    # Leyendo línea a línea lo que nos envía el cliente
-		    line = self.rfile.read()
-		    print "El cliente nos manda " + line
+    def handle(self):
 
-		    # Si no hay más líneas salimos del bucle infinito
-		    if not line:
-		        break
-			
-			# Seleccionamos la respuesta correcta
+        self.wfile.write("Hemos recibido tu peticion")
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+            print "El cliente nos manda " + line
 
-			Metodo = line.split(" ")[0]
-			if Metodo == "INVITE" or Metodo == "ACK" or Metodo == "BYE":
-				if Metodo == "INVITE" and line.split(" ")[2] =='SIP/2.0\r\n\r\n':
-					Answer = "SIP/2.0 100 Trying\r\n\r\n"
-					Answer += "SIP/2.0 180 Ring\r\n\r\n"
-					Answer += "SIP/2.0 200 OK\r\n\r\n"
-			
-				elif Metodo == "ACK":
-					# Tratamiento ACK
-					aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + FICHERO_AUDIO
-					print "Vamos a ejecutar", aEjecutar
-					os.system (aEjecutar)
-				elif Metodo == "BYE":
-					# Tratamiento BYE
-					Answer = "SIP/2.0 200 OK\r\n\r\n"
-				else:	
-					Answer = "SIP/2.0 400 Bad Request"
-			else:
-				Answer = "SIP/2.0 405 Method Not Allowed"
-		
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
 
-			# Imprimimos la respuesta y la enviamos
+            # Seleccionamos la respuesta correcta
 
-			if  Metodo != "ACK":
-				print "Enviamos:" + Answer
-				self.wfile.write(Answer)
+            Metodo = line.split(" ")[0]
+            if Metodo == "INVITE" or Metodo == "ACK" or Metodo == "BYE":
+                if Metodo == "INVITE" and \
+                        line.split(" ")[2] == 'SIP/2.0\r\n\r\n':
+
+                    Answer = "SIP/2.0 100 Trying\r\n\r\n"
+                    Answer += "SIP/2.0 180 Ring\r\n\r\n"
+                    Answer += "SIP/2.0 200 OK\r\n\r\n"
+
+                elif Metodo == "ACK":
+                    # Tratamiento ACK
+                    aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + \
+                        FICHERO_AUDIO
+                    print "Vamos a ejecutar", aEjecutar
+                    os.system(aEjecutar)
+                elif Metodo == "BYE":
+                    # Tratamiento BYE
+                    Answer = "SIP/2.0 200 OK\r\n\r\n"
+                else:
+                    Answer = "SIP/2.0 400 Bad Request"
+            else:
+                Answer = "SIP/2.0 405 Method Not Allowed"
+
+            # Imprimimos la respuesta y la enviamos
+
+            if Metodo != "ACK":
+                print "Enviamos:" + Answer
+                self.wfile.write(Answer)
 
 if __name__ == "__main__":
-
 
     # Creamos servidor de eco y escuchamos
 
