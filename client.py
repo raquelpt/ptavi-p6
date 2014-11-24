0efa97b16f6f488f7255d2a2426ebcd71f95a2f7
raquelpt <raquelita_25_4@hotmail.com>
2014-11-24 12:06:34 +0100

    Update client.py

--- client.py
+++ client.py
@@ -7,11 +7,16 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
+# Comprobamos si es correcto el numero de argumentos pasados
+
+if len(sys.argv) != 3:
+	print "Usage: python client.py method receiver@IP:SIPport"
+	raise SystemExit
 
 # Cliente UDP simple.
 
 lista = sys.argv
-METODO = lista[1]
+METODO = lista[1].upper()
 Direccion = lista[2]
 
 # Dirección IP del servidor.
@@ -20,18 +25,28 @@ SERVER = Direccion[1].split("@").split(":")[0]
 PORT = int(Direccion.split("@")[1].split(":")[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+
+LINE =  METODO + " sip:" + Direccion.split(":")[0] + " SIP/2.0\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+try:
+	print "Enviando: " + LINE
+	my_socket.send(LINE)
+	data = my_socket.recv(1024)
+	except socket.error:
+	print 'Error: No server listening at ' + SERVER + ' port ' + PORT
+	raise SystemExit
 
-print 'Recibido -- ', data
+
+print 'Recibido --', data
+if METODO == 'INVITE':
+	LINE = "ACK sip:" + Direccion.split(":")[0] + " SIP/2.0\r\n\r\n"
+	my_socket.send(LINE)
+
 print "Terminando socket..."
 
 # Cerramos todo
